3339d0c17aa6592c4de33a1f778fd3b7d384fe12
tianyawy <tianya_wy@hotmail.com>
2016-12-09 08:48:07 +0800

    [victoryw] #n/a add success build

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -12,4 +12,9 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+
+    @Test
+    public void should_success_ci_build(){
+        assertTrue(false);
+    }
 }
